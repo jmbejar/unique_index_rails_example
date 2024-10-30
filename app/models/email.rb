42cd23faf354a2d90c653239d6812a1bc5c25155
Jorge Bejar <jorge@wyeworks.com>
2024-10-30 18:09:51 -0300

    Prepare attempt 1 example

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,6 +3,4 @@ class Email < ApplicationRecord
   validates :address, presence: true
   # Unique validation at the Rails level
   validates :address, uniqueness: true
-
-  self.ignored_columns += [ "address_old" ]
 end
--- db/migrate/20241029025418_add_missing_index_to_email_address_step_3.rb
+++
@@ -1,5 +0,0 @@
-class AddMissingIndexToEmailAddressStep3 < ActiveRecord::Migration[7.2]
-  def change
-    remove_column :emails, :address_old
-  end
-end
