1bb2ad60720ec82b4c6c7259419530746b21f035
Jorge Bejar <jorge@wyeworks.com>
2024-12-13 09:52:01 -0300

    Only setting the special config in a per-test basis

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
   test "should get not_found" do
+    puts "ErrorsControllerTest", Rails.application.env_config["action_dispatch.show_exceptions"]
     get errors_not_found_url
     assert_response :not_found
   end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    Rails.application.env_config["consider_all_requests_local"] = false
+    Rails.application.env_config["action_dispatch.show_exceptions"] = true
+  end
+
+  teardown do
+    Rails.application.env_config["consider_all_requests_local"] = true
+    Rails.application.env_config["action_dispatch.show_exceptions"] = false
+  end
+
   test "should index" do
+    puts "should index", Rails.application.env_config["action_dispatch.show_exceptions"]
     get users_url
     assert_response :error
   end
